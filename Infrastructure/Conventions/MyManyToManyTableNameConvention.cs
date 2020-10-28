0dae281b33f6e8c5179695e89b0afc3402ce9441
Marina Paley <marina_paley@mail.ru>
2020-10-29 01:27:36 +0300

    Правки по тех. долгу
     - Удалены неиспользуемые зависимости в пакетах.
     - Частично удовлетворён StyleCop

--- Infrastructure/Conventions/MyManyToManyTableNameConvention.cs
+++ Infrastructure/Conventions/MyManyToManyTableNameConvention.cs
@@ -1,23 +1,25 @@
 ﻿namespace Infrastructure.Conventions
 {
-    using System;
-
     using FluentNHibernate.Conventions;
     using FluentNHibernate.Conventions.Inspections;
 
+    /// <summary>
+    /// Собственная конвенция по наименованию промежуточной сущности в связи многие-ко-многим.
+    /// </summary>
     internal class MyManyToManyTableNameConvention : ManyToManyTableNameConvention
     {
-        protected override string GetBiDirectionalTableName(IManyToManyCollectionInspector collection, IManyToManyCollectionInspector otherSide)
+        /// <inheritdoc />
+        protected override string GetBiDirectionalTableName(
+            IManyToManyCollectionInspector collection,
+            IManyToManyCollectionInspector otherSide)
         {
-            var tmp = $"{collection.ChildType.Name}_{otherSide.ChildType.Name}";
-            Console.WriteLine(tmp);
-            return tmp;
+            return $"{collection.ChildType.Name}_{otherSide.ChildType.Name}";
         }
 
+        /// <inheritdoc />
         protected override string GetUniDirectionalTableName(IManyToManyCollectionInspector collection)
         {
-            var tmp = $"{collection.ChildType.Name}_{collection.OtherSide.ChildType.Name}";
-            return tmp;
+            return $"{collection.ChildType.Name}_{collection.OtherSide.ChildType.Name}";
         }
     }
 }
